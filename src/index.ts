67aac7b3aaaadd91c7271665f5203ae2b8f13547
Shivam <shivamrai6836@gmail.com>
2025-12-07 12:50:16 -0500

    health api has been updated

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ app.get("/", (req, res)=>{
     res.send("Websocket server is working");
 });
 
-app.get("/health", (req, res)=>{
+app.get("/api/v1/health", (req, res)=>{
     res.status(200).json({
         status: "healthy",
         service: "Real-time Event Gateway",
