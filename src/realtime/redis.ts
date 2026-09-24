7f9ed56263c2fad350c5a0312adbdce6031fd847
Shivam <shivamrai6836@gmail.com>
2026-09-24 05:01:03 -0400

    accept host:port redis url

--- src/realtime/redis.ts
+++ src/realtime/redis.ts
@@ -1,12 +1,15 @@
 import { createClient } from "redis";
 
+const [redisHost, redisPortText] = (process.env.REDIS_URL || "localhost").split(":");
+const redisPort = Number(redisPortText) || Number(process.env.REDIS_PORT) || 16296;
+
 export const createRedisClient = () => {
     const client = createClient({
         username: "default",
         password: process.env.REDIS_PASSWORD || "",
         socket: {
-            host: process.env.REDIS_URL || "localhost",
-            port: Number(process.env.REDIS_PORT) || 16296,
+            host: redisHost,
+            port: redisPort,
         },
     });
     client.on("error", (err) => console.error("Redis Client Error", err));
